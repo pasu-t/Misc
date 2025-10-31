a2bbc7db03ab8b29b2503da45cee6b819d22db8a
pasu-t <46959852+pasu-t@users.noreply.github.com>
2025-10-31 05:31:27 +0530

    Delete groovy_practice directory

--- groovy_practice/oops/Dog.groovy
+++
@@ -1,5 +0,0 @@
-//package oops
-
-class Dog extends Animal {
-      
-}
--- groovy_practice/regexDemo.groovy
+++
@@ -1,8 +0,0 @@
-def regex = /(?:[^Groovy]*)/
-def match = "This is Groovy" =~ regex
-
-if(match){
-   println match[0]
-}else {
-   println "no match found"
-}
